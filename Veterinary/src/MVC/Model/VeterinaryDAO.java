d38ead058df36cc337944b735a6ace67c09068a9
zu0910 <zullyortiz.09@gmail.com>
2025-03-22 17:14:53 -0500

    feat :sparkles: database created with the java project

---
+++ Veterinary/src/MVC/Model/VeterinaryDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MVC.Model;
+
+import java.sql.*;
+
+public class VeterinaryDAO {
+    
+    // Database Connection Attributes
+    
+    private String URL = "";
+    private String USER = "root";
+    private String PASSWORD = "";
+}
