2cad7b88f3f8b189c5240a27f56773a7ca9bd936
zu0910 <zullyortiz.09@gmail.com>
2025-03-31 17:10:14 -0500

    feat:sparkles: Start whith Medications DAO

--- Veterinary/src/MVC/Controller/ControllerMedica.java
+++ Veterinary/src/MVC/Controller/ControllerMedica.java
@@ -4,10 +4,52 @@
  */
 package MVC.Controller;
 
-/**
- *
- * @author const
- */
-public class ControllerMedica {
+import MVC.Model.MedicationDAO;
+import MVC.Model.medications;
+import MVC.View.crudMedication;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+
+public class ControllerMedica implements ActionListener{
+    
+    medications medi = new medications();
+    MedicationDAO dao = new MedicationDAO();
+    crudMedication view = new crudMedication();
+    DefaultTableModel model = new DefaultTableModel();
+
+    public ControllerMedica(medications medi,MedicationDAO dao,crudMedication view ) {
+        this.medi = medi;
+        this.dao = dao;
+        this.view = view;
+        
+        this.view.btnDeleteMedi.addActionListener(this);
+        this.view.btnInsertMedi.addActionListener(this);
+        this.view.btnListarMedi.addActionListener(this);
+        this.view.btnUpdateMedi.addActionListener(this);
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource() == view.btnInsertMedi){
+            medi.setNameMedication(view.txtNameMedi.getText());
+            medi.setTypeMedication(view.txtTypeMedi.getText());
+            medi.setManufacturer(view.txtManuMedi.getText());
+            medi.setQuantity_in_stock(Integer.parseInt(view.txtStockMedi.getText()));
+            medi.setExpiration_date(view.txtExpiMedi.getText());
+            medi.setQuantity_entered(Integer.parseInt(view.txtEntereMedi.getText()));
+            medi.setId_supplier(Integer.parseInt(view.txtSupMedi.getText()));
+            if(dao.InsertMedi(medi)){
+                JOptionPane.showMessageDialog(null, "User add");
+            }else{
+                JOptionPane.showMessageDialog(null, "User not add");
+            }
+        }
+        
+    }
     
 }
--- Veterinary/src/MVC/View/crudMedication.java
+++ Veterinary/src/MVC/View/crudMedication.java
@@ -327,13 +327,13 @@ public class crudMedication extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(crudInventary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(crudMedication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(crudInventary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(crudMedication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(crudInventary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(crudMedication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(crudInventary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(crudMedication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
